53cc3ef323c5703a6dd706540b0ec0d407278180
ruslanzverkov <createmed@mail.ru>
2022-11-29 19:44:55 +0300

    Initial commit

---
+++ src/main/java/com/example/kursovaja2/Kursovaja2Application.java
@@ -0,0 +1,13 @@
+package com.example.kursovaja2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kursovaja2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kursovaja2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kursovaja2/Kursovaja2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kursovaja2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kursovaja2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
